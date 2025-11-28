4f3e3178f62f3dfd981f51e0172aa8b3053f6f4f
DanielRabinovitz <danielrabinovitz+github@gmail.com>
2025-11-27 21:56:04 -0500

    added tutorial popups in main game scene

--- Assets/Scripts/Tutorial popup.cs
+++ Assets/Scripts/Tutorial popup.cs
@@ -1,25 +1,83 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TutorialPopup : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class TutorialPopup : MonoBehaviour
 {
-    public GameObject popupPanel;
+    [Header("Buttons")]
+    public GameObject howToPlayButton;
+    public GameObject noteDetailsButton;
+
+    [Header("Popup Elements")]
+    public GameObject whiteBG;
+    public GameObject howToPlayText;
+    public GameObject noteDetailsText;
 
     void Start()
     {
-        if (popupPanel != null)
-            popupPanel.SetActive(false);
+        // Hide all popup elements initially
+        if (whiteBG != null)
+            whiteBG.SetActive(false);
+        if (howToPlayText != null)
+            howToPlayText.SetActive(false);
+        if (noteDetailsText != null)
+            noteDetailsText.SetActive(false);
+
+        // Add event triggers to buttons
+        SetupButtonHoverEvents(howToPlayButton, ShowHowToPlayPopup, HideAllPopups);
+        SetupButtonHoverEvents(noteDetailsButton, ShowNoteDetailsPopup, HideAllPopups);
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    void SetupButtonHoverEvents(GameObject button, UnityEngine.Events.UnityAction onEnter, UnityEngine.Events.UnityAction onExit)
     {
-        if (popupPanel != null)
-            popupPanel.SetActive(true);
+        if (button == null) return;
+
+        EventTrigger trigger = button.GetComponent<EventTrigger>();
+        if (trigger == null)
+            trigger = button.AddComponent<EventTrigger>();
+
+        // Clear existing entries to avoid duplicates
+        trigger.triggers.Clear();
+
+        // Add PointerEnter event
+        EventTrigger.Entry entryEnter = new EventTrigger.Entry();
+        entryEnter.eventID = EventTriggerType.PointerEnter;
+        entryEnter.callback.AddListener((data) => { onEnter(); });
+        trigger.triggers.Add(entryEnter);
+
+        // Add PointerExit event
+        EventTrigger.Entry entryExit = new EventTrigger.Entry();
+        entryExit.eventID = EventTriggerType.PointerExit;
+        entryExit.callback.AddListener((data) => { onExit(); });
+        trigger.triggers.Add(entryExit);
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    void ShowHowToPlayPopup()
     {
-        if (popupPanel != null)
-            popupPanel.SetActive(false);
+        if (whiteBG != null)
+            whiteBG.SetActive(true);
+        if (howToPlayText != null)
+            howToPlayText.SetActive(true);
+        if (noteDetailsText != null)
+            noteDetailsText.SetActive(false);
+    }
+
+    void ShowNoteDetailsPopup()
+    {
+        if (whiteBG != null)
+            whiteBG.SetActive(true);
+        if (noteDetailsText != null)
+            noteDetailsText.SetActive(true);
+        if (howToPlayText != null)
+            howToPlayText.SetActive(false);
+    }
+
+    void HideAllPopups()
+    {
+        if (whiteBG != null)
+            whiteBG.SetActive(false);
+        if (howToPlayText != null)
+            howToPlayText.SetActive(false);
+        if (noteDetailsText != null)
+            noteDetailsText.SetActive(false);
     }
 }
